55e3e28578c1a293698b1132957d2ab3a9ef2e4f
David McDonald <idavidmcdonald@me.com>
2018-08-14 16:44:30 +0100

    Include env files for staging and prod environments
    Deployment has been slowed as these variables have not been known
    or takes a bit of time to dig them out from elsewhere. This should
    make things quicker.
    
    Note, `PROFILE_NAME` is likely to differ by each developer as it
    is the name you deceided on for your AWS vault profiles. I've
    included mine for the moment which is likely not ideal but
    may be an improvement to start with.

---
+++ environment_production.sh
@@ -0,0 +1,4 @@
+export TERRAFORM_BUCKET="prometheus-production"
+export PROFILE_NAME="gds-prometheus-prod" # This may depend on your local AWS vault set up
+export ENV="production"
+export DEV_ENVIRONMENT=false
