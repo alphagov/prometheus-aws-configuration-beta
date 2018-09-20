609940550053dd6e5336b79447545ec1591e6b88
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2018-09-20 16:00:12 +0100

    add tool to scrape all queries from grafana
    This adds a python utility to scrape the grafana API and print all
    PromQL queries, along with the dashboard and panel that the query came
    from.
    
    I thought this might be useful when we're looking for usages of a
    label, or if we discover a problematic query pattern and want to find
    usages of it.

---
+++ tools/show-queries/show_queries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from grafana_api.grafana_api import GrafanaAPI
+from bearer_auth import BearerAuth
+import os, sys
+
+
+def exprs_for_dashboard(dashboard):
+    d = g.get('/dashboards/uid/%s' % dashboard['uid'])
+    panels = d['dashboard']['panels']
+    for panel in panels:
+        targets = panel.get('targets',[])
+        for target in targets:
+            if 'expr' in target:
+                yield (target['expr'], dashboard['title'], panel['title'])
+
+
+if __name__ == "__main__":
+    try:
+        token = os.environ['GRAFANA_TOKEN']
+        g = GrafanaAPI(BearerAuth(token), 'grafana-paas.cloudapps.digital', protocol='https')
+        dashboards = g.get('/search?type=dash-db')
+        exprs = [expr for dashboard in dashboards for expr in exprs_for_dashboard(dashboard)]
+        exprs.sort()
+        for expr in exprs:
+            print(expr)
+    except KeyError as e:
+        print('Please set the %s environment variable' % e.args[0], file=sys.stderr)
+        exit(1)
+
