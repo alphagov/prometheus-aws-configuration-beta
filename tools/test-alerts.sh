e148fb5e5bbcf33f595a7b04cac04d8b00ef8cf7
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2018-10-04 13:54:19 +0100

    Add alerts tests for observe alerts and shell script
    - providing no args to the test-alerts.sh shell script will find and run all the tests
    - providing an arg will target a particular test alert

---
+++ tools/test-alerts.sh
@@ -0,0 +1,15 @@
+echo "Usage: <arg = team name, eg observe> | no args to run all alert tests"
+
+# find the matching team alerts or all the alerts test files
+TEAM="$1"
+if [ -z "${1}" ] ; then
+    TEAM="*"
+fi
+
+ALERTS_PATH="terraform/projects/app-ecs-services/config/alerts-tests"
+ALERTS_DIR=($(find $ALERTS_PATH -name test-$TEAM-alerts.yml -not -name 'test-template.yml'))
+
+# run promtool test against each alerts test file found
+for ALERTS_TEST in "${ALERTS_DIR[@]}"; do
+    promtool test rules $ALERTS_TEST
+done
