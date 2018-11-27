28b3c0adacb1dd65805a1ffd8ac1f5d1acd0b75f
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2018-11-27 18:09:31 +0000

    tighten node_exporter sg rule
    We don't need to allow 9100 from 10.0.0.0/16, we only need to allow it
    from all the other prometheis (which are in the same security group).

--- terraform/modules/enclave/prometheus/main.tf
+++ terraform/modules/enclave/prometheus/main.tf
@@ -105,12 +105,12 @@ resource "aws_security_group_rule" "allow_prometheus_private" {
 }
 
 resource "aws_security_group_rule" "allow_prometheus_node_exporter" {
-  security_group_id = "${aws_security_group.allow_prometheus.id}"
-  type              = "ingress"
-  from_port         = 9100
-  to_port           = 9100
-  protocol          = "tcp"
-  cidr_blocks       = ["10.0.0.0/16"]
+  security_group_id        = "${aws_security_group.allow_prometheus.id}"
+  type                     = "ingress"
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.allow_prometheus.id}"
 }
 
 resource "aws_security_group" "allow_prometheus" {
