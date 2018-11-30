02a93950757be6a3ab0c8244d3d95b444a659ad8
Matthew Cullum <matthew.cullum@digital.cabinet-office.gov.uk>
2018-11-30 14:15:15 +0000

    Refactor app-ecs-instances into a module and per-environment projects

---
+++ terraform/projects/app-ecs-instances-production/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = "= 0.11.10"
+
+  backend "s3" {
+    bucket = "prometheus-production"
+    key    = "app-ecs-instances-modular.tfstate"
+  }
+}
+
+provider "aws" {
+  version = "~> 1.14.1"
+  region  = "${var.aws_region}"
+}
+
+variable "aws_region" {
+  type        = "string"
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "stack_name" {
+  type        = "string"
+  description = "Unique name for this collection of resources"
+  default     = "production"
+}
+
+variable "project" {
+  type        = "string"
+  description = "Project name for tag"
+  default     = "app-ecs-instances-production"
+}
+
+variable "remote_state_bucket" {
+  type        = "string"
+  description = "S3 bucket we store our terraform state in"
+  default     = "prometheus-production"
+}
+
+module "app-ecs-instances" {
+  source = "../../modules/app-ecs-instances"
+
+  dev_environment     = false
+  stack_name          = "${var.stack_name}"
+  project             = "${var.project}"
+  remote_state_bucket = "${var.remote_state_bucket}"
+}
---
+++ terraform/projects/app-ecs-instances-staging/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = "= 0.11.10"
+
+  backend "s3" {
+    bucket = "prometheus-staging"
+    key    = "app-ecs-instances-modular.tfstate"
+  }
+}
+
+provider "aws" {
+  version = "~> 1.14.1"
+  region  = "${var.aws_region}"
+}
+
+variable "aws_region" {
+  type        = "string"
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "stack_name" {
+  type        = "string"
+  description = "Unique name for this collection of resources"
+  default     = "staging"
+}
+
+variable "project" {
+  type        = "string"
+  description = "Project name for tag"
+  default     = "app-ecs-instances-staging"
+}
+
+variable "remote_state_bucket" {
+  type        = "string"
+  description = "S3 bucket we store our terraform state in"
+  default     = "prometheus-staging"
+}
+
+module "app-ecs-instances" {
+  source = "../../modules/app-ecs-instances"
+
+  dev_environment     = false
+  stack_name          = "${var.stack_name}"
+  project             = "${var.project}"
+  remote_state_bucket = "${var.remote_state_bucket}"
+}
