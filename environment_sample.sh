7dc71cb49684db39689c8507ec6ac4ba28a0c03e
Isabell (Issy) Long <me@issyl0.co.uk>
2018-12-04 15:27:31 +0000

    Delete environment_{environment}.sh deployment scripts

--- environment_sample.sh
+++
@@ -1,5 +0,0 @@
-export TERRAFORM_BUCKET=<your-bucket>
-export PROFILE_NAME=gds-tech-ops
-export ENV=<your-env>
-export DEV_ENVIRONMENT=true
-export DEV_TICKET_RECIPIENT_EMAIL=$(git config user.email) # or whatever email address you want to receive ticket notifications on
