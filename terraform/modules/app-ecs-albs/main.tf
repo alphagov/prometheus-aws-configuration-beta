abb8d75ccb701836e08dc829e66f3f2c0997ab3e
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2018-12-14 20:34:16 +0000

    Add new alertmanager ALB (not in use yet)
    This adds a new alertmanager ALB.  This is different from the existing
    one because:
    
     - it's designed to directly serve alertmanager, not go via the nginx
       auth-proxy
     - it's IP restricted at the security group layer
    
    It's not wired up yet.  To wire it up, we need to:
    
     - change the alerts_alias route53 record to point at the new ALB
       instead of the old
     - change the alertmanager ECS service to point at the
       alertmanager_target_group_ids instead of monitoring_internal_tg
    
    The switchover will cause downtime, but only to users of the
    alertmanager interface from office IPs, which is not a critical use
    case.

--- terraform/modules/app-ecs-albs/main.tf
+++ terraform/modules/app-ecs-albs/main.tf
@@ -53,6 +53,21 @@ variable "alertmanager_count" {
   default     = "3"
 }
 
+variable "alerts_allowed_cidrs" {
+  type        = "list"
+  description = "List of CIDRs which are able to access the alertmanager ALB, default are GDS ips"
+
+  default = [
+    "213.86.153.212/32",
+    "213.86.153.213/32",
+    "213.86.153.214/32",
+    "213.86.153.235/32",
+    "213.86.153.236/32",
+    "213.86.153.237/32",
+    "85.133.67.244/32",
+  ]
+}
+
 # locals
 # --------------------------------------------------------------
 
@@ -136,40 +151,6 @@ resource "aws_lb" "monitoring_internal_alb" {
     map("Stackname", "${var.stack_name}"),
     map("Name", "${var.stack_name}-monitoring-internal-${count.index + 1}")
   )}"
-}
-
-# AWS should manage the certificate renewal automatically
-# https://docs.aws.amazon.com/acm/latest/userguide/managed-renewal.html
-# If this fails, AWS will email associated with the AWS account
-resource "aws_acm_certificate" "alertmanager_cert" {
-  domain_name       = "alerts.${local.subdomain}"
-  validation_method = "DNS"
-
-  subject_alternative_names = ["${aws_route53_record.alerts_alias.*.fqdn}"]
-
-  lifecycle {
-    # We can't destroy a certificate that's in use, and we can't stop
-    # using it until the new one is ready.  Hence
-    # create_before_destroy here.
-    create_before_destroy = true
-  }
-}
-
-resource "aws_route53_record" "alertmanager_cert_validation" {
-  # Count matches the domain_name plus each `subject_alternative_domain`
-  count = "${1 + local.alerts_records_count}"
-
-  name       = "${lookup(aws_acm_certificate.alertmanager_cert.domain_validation_options[count.index], "resource_record_name")}"
-  type       = "${lookup(aws_acm_certificate.alertmanager_cert.domain_validation_options[count.index], "resource_record_type")}"
-  zone_id    = "${var.zone_id}"
-  records    = ["${lookup(aws_acm_certificate.alertmanager_cert.domain_validation_options[count.index], "resource_record_value")}"]
-  ttl        = 60
-  depends_on = ["aws_acm_certificate.alertmanager_cert"]
-}
-
-resource "aws_acm_certificate_validation" "alertmanager_cert" {
-  certificate_arn         = "${aws_acm_certificate.alertmanager_cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.alertmanager_cert_validation.*.fqdn}"]
 }
 
 resource "aws_route53_record" "alerts_alias" {
@@ -364,6 +345,9 @@ resource "aws_security_group" "prometheus_alb" {
   vpc_id      = "${local.vpc_id}"
 }
 
+# Prometheus is fronted by an nginx which controls access to
+# either approved IP addresses, or users with basic auth creds
+# so we need to allow all IPs here
 resource "aws_security_group_rule" "prom_allow_http" {
   security_group_id = "${aws_security_group.prometheus_alb.id}"
   type              = "ingress"
@@ -478,6 +462,183 @@ resource "aws_lb_target_group" "prometheus_tg" {
   }
 }
 
+######################################################################
+# ----- alertmanager public ALB -------
+######################################################################
+
+# AWS should manage the certificate renewal automatically
+# https://docs.aws.amazon.com/acm/latest/userguide/managed-renewal.html
+# If this fails, AWS will email associated with the AWS account
+resource "aws_acm_certificate" "alertmanager_cert" {
+  domain_name       = "alerts.${local.subdomain}"
+  validation_method = "DNS"
+
+  subject_alternative_names = ["${aws_route53_record.alerts_alias.*.fqdn}"]
+
+  lifecycle {
+    # We can't destroy a certificate that's in use, and we can't stop
+    # using it until the new one is ready.  Hence
+    # create_before_destroy here.
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "alertmanager_cert_validation" {
+  # Count matches the domain_name plus each `subject_alternative_domain`
+  count = "${1 + local.alerts_records_count}"
+
+  name       = "${lookup(aws_acm_certificate.alertmanager_cert.domain_validation_options[count.index], "resource_record_name")}"
+  type       = "${lookup(aws_acm_certificate.alertmanager_cert.domain_validation_options[count.index], "resource_record_type")}"
+  zone_id    = "${var.zone_id}"
+  records    = ["${lookup(aws_acm_certificate.alertmanager_cert.domain_validation_options[count.index], "resource_record_value")}"]
+  ttl        = 60
+  depends_on = ["aws_acm_certificate.alertmanager_cert"]
+}
+
+resource "aws_acm_certificate_validation" "alertmanager_cert" {
+  certificate_arn         = "${aws_acm_certificate.alertmanager_cert.arn}"
+  validation_record_fqdns = ["${aws_route53_record.alertmanager_cert_validation.*.fqdn}"]
+}
+
+#### Uncomment this and remove the other alerts_alias when switching
+#### over from the old ALB
+#
+# resource "aws_route53_record" "alerts_alias" {
+#   count = "${local.alerts_records_count}"
+
+#   zone_id = "${var.zone_id}"
+#   name    = "alerts-${count.index + 1}"
+#   type    = "A"
+
+#   alias {
+#     name                   = "${aws_lb.alertmanager_alb.dns_name}"
+#     zone_id                = "${aws_lb.alertmanager_alb.zone_id}"
+#     evaluate_target_health = false
+#   }
+# }
+
+resource "aws_security_group" "alertmanager_alb" {
+  name        = "${var.stack_name}-alertmanager-alb"
+  description = "Access to alertmanager ALB (${var.stack_name})"
+  vpc_id      = "${local.vpc_id}"
+}
+
+# Unlike prometheus, alertmanager is IP restricted by security group
+# here
+resource "aws_security_group_rule" "alerts_allow_http" {
+  security_group_id = "${aws_security_group.alertmanager_alb.id}"
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.alerts_allowed_cidrs}"]
+}
+
+resource "aws_security_group_rule" "alerts_allow_https" {
+  security_group_id = "${aws_security_group.alertmanager_alb.id}"
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.alerts_allowed_cidrs}"]
+}
+
+resource "aws_lb" "alertmanager_alb" {
+  name               = "${var.stack_name}-alertmanager-alb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [
+    "${data.terraform_remote_state.infra_security_groups.monitoring_external_sg_id}",
+    "${aws_security_group.alertmanager_alb.id}",
+  ]
+
+  subnets = [
+    "${var.subnets}",
+  ]
+
+  tags = "${merge(
+    local.default_tags,
+    var.additional_tags,
+    map("Stackname", "${var.stack_name}"),
+    map("Name", "${var.stack_name}-alertmanager-alb")
+  )}"
+}
+
+resource "aws_lb_listener" "alertmanager_listener_http" {
+  load_balancer_arn = "${aws_lb.alertmanager_alb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "alertmanager_listener_https" {
+  load_balancer_arn = "${aws_lb.alertmanager_alb.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = "${aws_acm_certificate_validation.alertmanager_cert.certificate_arn}"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Not found"
+      status_code  = "404"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "alerts_listener_https" {
+  count = "${var.alertmanager_count}"
+
+  listener_arn = "${aws_lb_listener.alertmanager_listener_https.arn}"
+  priority     = "${100 + count.index}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${element(aws_lb_target_group.alertmanager_tg.*.arn, count.index)}"
+  }
+
+  condition {
+    field = "host-header"
+
+    values = [
+      "alerts-${count.index + 1}.*",
+    ]
+  }
+}
+
+resource "aws_lb_target_group" "alertmanager_tg" {
+  count = "${var.alertmanager_count}"
+
+  name                 = "${var.stack_name}-alerts-${count.index +1}-tg"
+  port                 = 80
+  protocol             = "HTTP"
+  vpc_id               = "${local.vpc_id}"
+  deregistration_delay = 30
+
+  health_check {
+    interval            = "10"
+    path                = "/health" # static health check on nginx auth proxy
+    matcher             = "200"
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = "5"
+  }
+}
+
 ## Outputs
 
 output "monitoring_external_tg" {
@@ -533,4 +694,9 @@ output "prom_private_record_fqdns" {
 output "prometheus_target_group_ids" {
   value       = "${aws_lb_target_group.prometheus_tg.*.arn}"
   description = "Prometheus target group IDs"
+}
+
+output "alertmanager_target_group_ids" {
+  value       = "${aws_lb_target_group.alertmanager_tg.*.arn}"
+  description = "Alertmanager target group IDs"
 }
