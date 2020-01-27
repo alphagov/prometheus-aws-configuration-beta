48a9938f7bc63f17bcfa96e1d54f59fa9f9e5211
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2020-01-27 20:10:27 +0000

    remove unused variable

--- terraform/modules/app-ecs-services/main.tf
+++ terraform/modules/app-ecs-services/main.tf
@@ -11,21 +11,6 @@ variable "aws_region" {
   default     = "eu-west-1"
 }
 
-variable "cidr_admin_whitelist" {
-  description = "CIDR ranges permitted to communicate with administrative endpoints"
-  type        = list(string)
-
-  default = [
-    "213.86.153.212/32",
-    "213.86.153.213/32",
-    "213.86.153.214/32",
-    "213.86.153.235/32",
-    "213.86.153.236/32",
-    "213.86.153.237/32",
-    "85.133.67.244/32",
-  ]
-}
-
 variable "remote_state_bucket" {
   type        = string
   description = "S3 bucket we store our terraform state in"
