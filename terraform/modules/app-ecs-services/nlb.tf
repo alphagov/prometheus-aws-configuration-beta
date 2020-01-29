e4448bacd9d19c482cf5c0d1a0d53ec3b5452301
Sam Crang <sam.crang@gmail.com>
2020-01-29 16:24:51 +0000

    Remove health check features not supported by NLBs

--- terraform/modules/app-ecs-services/nlb.tf
+++ terraform/modules/app-ecs-services/nlb.tf
@@ -99,12 +99,9 @@ resource "aws_lb_target_group" "alertmanager" {
 
   health_check {
     interval            = 10
-    path                = "/"
-    matcher             = "200"
-    protocol            = "HTTP"
+    protocol            = "TCP"
     healthy_threshold   = 2
     unhealthy_threshold = 2
-    timeout             = 5
   }
 }
 
