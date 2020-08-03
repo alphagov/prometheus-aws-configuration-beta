96cfca5e7d6229bb3df61d98d089b0de1eba722e
Philip Potter <philip.g.potter@gmail.com>
2020-08-03 10:26:30 +0100

    Update terraform/modules/prom-ec2/paas-config/main.tf

--- terraform/modules/prom-ec2/paas-config/main.tf
+++ terraform/modules/prom-ec2/paas-config/main.tf
@@ -10,7 +10,7 @@ locals {
   prometheus_config = yamldecode(data.template_file.prometheus_config_template.rendered)
   final_scrape_configs = concat(local.prometheus_config["scrape_configs"], var.extra_scrape_configs)
   final_prometheus_config = merge(local.prometheus_config, {"scrape_configs" = local.final_scrape_configs})
-  final_prometheus_config_yaml = yamlencode(final_prometheus_config)
+  final_prometheus_config_yaml = yamlencode(local.final_prometheus_config)
 }
 
 resource "aws_route53_record" "prom_ec2_a_record" {
