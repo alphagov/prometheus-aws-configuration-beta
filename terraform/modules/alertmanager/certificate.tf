3eb0dd13b0409085ec1c5608855d091a53b0dd37
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2020-08-18 16:35:11 +0100

    Remove old NLB and point `alerts.` name at ALB
    This completes the migration away from NLB.  All prometheis are
    sending alerts to the new ALB-based DNS names now, so the old NLB is
    no longer required.
    
    This is a busy commit but it's really doing 3 basic things:
    
     - deleting the NLB and associated listener and target group (causes
       terraform to destroy them)
     - moving the ACM certificate from nlb.tf to new file
       certificate.tf (terraform no-op)
     - moving the `alerts.` DNS alias from nlb.tf to alb.tf (causes
       terraform to update the DNS record)

---
+++ terraform/modules/alertmanager/certificate.tf
@@ -0,0 +1,42 @@
+# AWS should manage the certificate renewal automatically
+# https://docs.aws.amazon.com/acm/latest/userguide/managed-renewal.html
+# If this fails, AWS will email associated with the AWS account
+resource "aws_acm_certificate" "alertmanager_cert" {
+  domain_name       = "alerts.${local.subdomain}"
+  validation_method = "DNS"
+
+  subject_alternative_names = formatlist("alerts-%s.${local.subdomain}", data.aws_availability_zones.available.names)
+
+  lifecycle {
+    # We can't destroy a certificate that's in use, and we can't stop
+    # using it until the new one is ready.  Hence
+    # create_before_destroy here.
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "alertmanager_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.alertmanager_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
+  type    = each.value.type
+  zone_id = local.zone_id
+  ttl     = 60
+
+  allow_overwrite = true
+
+  depends_on = [aws_acm_certificate.alertmanager_cert]
+}
+
+resource "aws_acm_certificate_validation" "alertmanager_cert" {
+  certificate_arn         = aws_acm_certificate.alertmanager_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.alertmanager_cert_validation : record.fqdn]
+}
+
