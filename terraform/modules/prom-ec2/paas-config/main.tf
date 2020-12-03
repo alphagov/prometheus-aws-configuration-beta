d23fd16a9d3164b096900ad6dbcfeebe6c4313d5
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-12-03 13:23:05 +0000

    Add alerts for GOV.UK Accounts

--- terraform/modules/prom-ec2/paas-config/main.tf
+++ terraform/modules/prom-ec2/paas-config/main.tf
@@ -45,6 +45,13 @@ resource "aws_s3_bucket_object" "alerts-data-gov-uk-config" {
   etag   = filemd5("${var.alerts_path}data-gov-uk-alerts.yml")
 }
 
+resource "aws_s3_bucket_object" "alerts-gov-uk-accounts-config" {
+  bucket = var.prometheus_config_bucket
+  key    = "prometheus/alerts/gov-uk-accounts-alerts.yml"
+  source = "${var.alerts_path}gov-uk-accounts-alerts.yml"
+  etag   = filemd5("${var.alerts_path}gov-uk-accounts-alerts.yml")
+}
+
 resource "aws_s3_bucket_object" "alerts-notify-config" {
   bucket = var.prometheus_config_bucket
   key    = "prometheus/alerts/notify-alerts.yml"
